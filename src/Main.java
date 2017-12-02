0313333f74765287f15ae09ad6547aaaaf02fc57
kklt <1321210605@qq.com>
2017-12-02 09:40:47 +0800

    测试volatile的原子性

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,27 @@ public class Main {
 
      //this is first code
 	 //addnotice
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
+    public volatile int inc = 0;
+
+    public void increase() {
+        inc++;
     }
+
+    public static void main(String[] args) {
+        final Main main = new Main();
+        for(int i=0;i<10;i++){
+            new Thread(){
+                public void run() {
+                    for(int j=0;j<1000;j++)
+                        main.increase();
+                }
+            }.start();
+        }
+
+        while(Thread.activeCount()>1)  //保证前面的线程都执行完
+            Thread.yield();
+        System.out.println(main.inc);
+    }
+
 }
+
